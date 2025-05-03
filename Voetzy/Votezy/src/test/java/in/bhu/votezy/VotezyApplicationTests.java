cb30e9e892b465d70e19d394d6e602f47f1363a7
BhupendraChouhan2003 <153222718+BhupendraChouhan2003@users.noreply.github.com>
2025-05-03 12:09:41 +0530

    Add files via upload

---
+++ Voetzy/Votezy/src/test/java/in/bhu/votezy/VotezyApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bhu.votezy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotezyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
